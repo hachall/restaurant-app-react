039dbf416cbc4cfeebcfdfcfe81f06c5f7203a52
Hamish Hall <hachall@hotmail.com>
2020-02-18 17:43:32 +0000

    node modules bug - commiting to branch to investigate

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 
+// import { GiHamburger } from "react-icons/gi";
+
 class NavBar extends Component {
 
 
   render() {
     return (
-      <div className="navbar"></div>
+      <div className="navbar">
+        <GiHamburger />
+      </div>
     )
   }
 }
