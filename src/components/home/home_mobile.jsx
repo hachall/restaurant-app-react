9142747d6ea82ab7c23d961384ab0e0648992973
Hamish Hall <hachall@hotmail.com>
2020-05-10 21:53:20 +0100

    missing dependency

--- src/components/home/home_mobile.jsx
+++ src/components/home/home_mobile.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, lazy, Suspense } from 'react'
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
