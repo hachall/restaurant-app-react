285feb43ecc587e35c451eda787c7651bd34d585
Hamish Hall <hachall@hotmail.com>
2020-05-17 18:04:41 +0100

    just url

--- src/components/basket/checkout.jsx
+++ src/components/basket/checkout.jsx
@@ -24,7 +24,7 @@ class Checkout extends Component {
 
 
   render() {
-    let base_url = "http://production.dolxjcfav4ei2.amplifyapp.com"
+    let base_url = "http://production.dolxjcfav4ei2.amplifyapp.com";
     // let base_url = "https://test.dolxjcfav4ei2.amplifyapp.com"
     // let base_url = "http://localhost:8081"
 
